bed403a7791e0cf3b1e9a6e881aa09ac9e6d9424
Tee_me2 <114180127+Timmydavid123@users.noreply.github.com>
2025-10-25 19:06:35 +0100

    hii

--- src/Pages/LandingPage.tsx
+++ src/Pages/LandingPage.tsx
@@ -25,7 +25,7 @@ const menuRef = useRef<HTMLDivElement>(null);
   { name: "Shop", href: "/shop" },
   { name: "Portfolio", href: "/portfolio" },
     { name: "Exhibition & Publications", href: "/exhibition" },
-    { name: "NFTS", href: "/https://foundation.app/@adisaolashile" },
+    { name: "NFTS", href: "https://foundation.app/@adisaolashile" },
   { name: "About", href: "/about" },
   { name: "Contact", href: "/contact" },
 ]
